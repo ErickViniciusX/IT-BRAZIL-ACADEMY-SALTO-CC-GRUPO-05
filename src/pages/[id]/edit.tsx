e41b0dbd3ae2047c3cd46bd05992ea52d49bc9ef
Lucas Aquino <lucasbaquino@icloud.com>
2023-10-16 21:03:25 -0300

    fix: path to Form

--- src/pages/[id]/edit.tsx
+++ src/pages/[id]/edit.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
-import Form from '../../src/components/Form'
+import Form from '../../components/Form'
 
 const fetcher = (url: string) =>
   fetch(url)
