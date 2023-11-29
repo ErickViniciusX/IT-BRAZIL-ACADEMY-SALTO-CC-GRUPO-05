f4ad87523502bc2465c0d1da2fd11ce4ef5203c3
Lucas Aquino <lucasbaquino@icloud.com>
2023-11-29 09:21:19 -0300

    Merge from branch

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,29 +1,36 @@
+import CreateProduct from "@/components/CreateProduct";
+import Head from "next/head";
 import Link from "next/link";
 import React, { Children } from "react";
 
-type MainLayoutProps = { 
-    children: React.ReactNode;
- }
+type MainLayoutProps = {
+  children: React.ReactNode;
+}
 
 
-export default function MainLayout ({children}:MainLayoutProps) {
-    return (
-        <>
-          <div className="top-bar">
-            <div className="nav">
-              <Link href="/">Inicio</Link>
-              <Link href="/new">Adicionar</Link>
-            </div>
-    
-            <img
-              id="title"
-              src="logo.png"
-              alt="pet care logo"
-            ></img>
-          </div>
-          <div className="wrapper grid">
-            {children}
-          </div>
-        </>
-      )
+export default function MainLayout({ children }: MainLayoutProps) {
+  return (
+    <>
+      <Head>
+        <title>StockMaster</title>
+        <link rel="shortcut icon" href="icon.ico" />
+      </Head>
+
+      <div className="top-bar">
+        <div className="nav">
+          <Link href="/">Inicio</Link>
+          <CreateProduct />
+        </div>
+
+        <img
+          id="title"
+          src="logo.png"
+          alt="pet care logo"
+        ></img>
+      </div>
+      <div className="wrapper grid">
+        {children}
+      </div>
+    </>
+  )
 }
